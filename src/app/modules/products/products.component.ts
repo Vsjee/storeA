84110bceb5cdbda136cdf8159076ed699b29eda6
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 14:46:08 -0500

    use products service

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetProductsService } from 'src/app/services';
+import { IProduct } from 'src/app/types';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  products: IProduct[] = [];
+
+  constructor(private productsService: GetProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data) => {
+      this.products = data.products;
+    });
+  }
+}

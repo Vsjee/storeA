e090afde0e12e0a783379699c0c579025fd79db2
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 09:50:52 -0500

    add home and products module

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ProductsRoutingModule],
 })
 export class ProductsModule {}

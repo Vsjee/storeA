b178b840194c81720d6ffe615465dd3adc9bc523
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 09:43:29 -0500

    add http module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NabvarComponent } from './components';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { NabvarComponent } from './components';
     AppRoutingModule,
     BrowserAnimationsModule,
     NabvarComponent,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

faec812041978e579a860e63666c7bbcda43ae18
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 15:28:44 -0500

    add not found route comp

---
+++ src/app/utils/not-found-route/not-found-route.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-not-found-route',
+  templateUrl: './not-found-route.component.html',
+  styleUrls: ['./not-found-route.component.scss'],
+})
+export class NotFoundRouteComponent {}

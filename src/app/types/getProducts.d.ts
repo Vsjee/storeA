df804f496e95549e28613e10a56883c3891100d2
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 11:56:45 -0500

    add product types

--- src/app/types/getProducts.d.ts
+++ src/app/types/getProducts.d.ts
@@ -15,3 +15,17 @@ export interface IProducts {
     }
   ];
 }
+
+export interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}

78b9e65fa2125abd96a9816e3f24ed867a8aef34
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 09:49:30 -0500

    add main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { publicRoutes } from './models';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: `${publicRoutes.HOME}`, pathMatch: 'full' },
+  {
+    path: `${publicRoutes.HOME}`,
+    loadChildren: () => import('./modules').then((m) => m.HomeModule),
+  },
+  {
+    path: `${publicRoutes.PRODUCTS}`,
+    loadChildren: () => import('./modules').then((m) => m.ProductsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

03c843d3d88217a71b02a3b028ebeda9f1a2d4a2
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-29 14:46:28 -0500

    use product card

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductCardComponent } from 'src/app/components';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [CommonModule, ProductsRoutingModule, ProductCardComponent],
 })
 export class ProductsModule {}

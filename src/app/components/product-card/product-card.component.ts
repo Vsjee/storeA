e3ce1effee8095be1f49de155d8ec2ab955901ff
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-30 16:19:47 -0500

    add nav to a product

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -3,10 +3,11 @@ import { IProduct } from 'src/app/types';
 import { MatCardModule } from '@angular/material/card';
 import { productInit } from 'src/app/models';
 import { MatButtonModule } from '@angular/material/button';
+import { ProductsRoutingModule } from 'src/app/modules/products/products-routing.module';
 
 @Component({
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, ProductsRoutingModule],
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],

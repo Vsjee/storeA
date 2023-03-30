37aa01a6e1adaed76e8c3f5337004d4c6f82b185
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-30 18:27:33 -0500

    add hotlistener for the navbar

--- src/app/components/nabvar/nabvar.component.ts
+++ src/app/components/nabvar/nabvar.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   selector: 'app-nabvar',
   templateUrl: './nabvar.component.html',
   styleUrls: ['./nabvar.component.scss'],
 })
-export class NabvarComponent {}
+export class NabvarComponent implements OnInit {
+  navToggle = false;
+  public getScreenWidth = window.innerWidth;
+
+  ngOnInit() {
+    this.getScreenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+
+    if (this.getScreenWidth > 650) {
+      this.navToggle = false;
+    } else {
+      this.navToggle = true;
+    }
+  }
+}

9d21f675821113f304ef5f2427a256c6d825f0cc
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-31 12:13:33 -0500

    add handle search

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { GetProductsService } from 'src/app/services';
 import { IProduct } from 'src/app/types';
 
@@ -11,6 +12,27 @@ export class ProductsComponent implements OnInit {
   products: IProduct[] = [];
 
   constructor(private productsService: GetProductsService) {}
+
+  search = new FormControl('');
+
+  handleSearch() {
+    this.productsService.getProducts().subscribe((data) => {
+      let currSearch: string;
+      if (this.search.value !== null) {
+        currSearch = this.search.value;
+
+        if (currSearch !== '') {
+          const filteredData = data.products.filter((item) =>
+            item.title.toLowerCase().includes(currSearch.toLowerCase())
+          );
+          this.products = filteredData;
+          this.search.setValue('');
+        } else {
+          this.products = data.products;
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.productsService.getProducts().subscribe((data) => {

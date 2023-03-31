cfd7271b8b3708bba8692d727c4057261b90da19
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-31 12:11:26 -0500

    add some mat modules

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductCardComponent } from 'src/app/components';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule, ProductCardComponent],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    ProductCardComponent,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ProductsModule {}

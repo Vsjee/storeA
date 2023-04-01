d4814820d32c4eafcc144a7cded27c29bde6d701
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-31 22:38:30 -0500

    add global search comp

---
+++ src/app/components/global-search/global-search.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { GetProductsService } from 'src/app/services';
+import { IProduct } from 'src/app/types';
+import { FilterPipe } from 'src/app/pipes';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule, FilterPipe, AppRoutingModule],
+  selector: 'app-global-search',
+  templateUrl: './global-search.component.html',
+  styleUrls: ['./global-search.component.scss'],
+})
+export class GlobalSearchComponent implements OnInit {
+  products: IProduct[] = [];
+
+  searchText = '';
+
+  @ViewChild('searchbar') searchbar?: ElementRef;
+
+  constructor(private productsService: GetProductsService) {}
+
+  closeSearchMenu() {
+    this.searchText = '';
+  }
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data) => {
+      this.products = data.products;
+    });
+  }
+}

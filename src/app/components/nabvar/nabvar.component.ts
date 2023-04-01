8fa3d2b5bb1f084e652cd9b14b30e022b00d8f0c
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-31 22:40:26 -0500

    use global search in the nav comp

--- src/app/components/nabvar/nabvar.component.ts
+++ src/app/components/nabvar/nabvar.component.ts
@@ -1,9 +1,11 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { GlobalSearchComponent } from '../global-search/global-search.component';
 
 @Component({
   standalone: true,
@@ -13,27 +15,11 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatButtonModule,
     MatInputModule,
+    AppRoutingModule,
+    GlobalSearchComponent,
   ],
   selector: 'app-nabvar',
   templateUrl: './nabvar.component.html',
   styleUrls: ['./nabvar.component.scss'],
 })
-export class NabvarComponent implements OnInit {
-  navToggle = false;
-  public getScreenWidth = window.innerWidth;
-
-  ngOnInit() {
-    this.getScreenWidth = window.innerWidth;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onWindowResize() {
-    this.getScreenWidth = window.innerWidth;
-
-    if (this.getScreenWidth > 650) {
-      this.navToggle = false;
-    } else {
-      this.navToggle = true;
-    }
-  }
-}
+export class NabvarComponent {}

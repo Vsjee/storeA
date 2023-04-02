80fcdfb4a9ec7ed2df866d3c316822334c4c15e0
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-02 14:37:17 -0500

    add carousel for each category

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetProductsService } from 'src/app/services';
+import { IProduct } from 'src/app/types';
+import { filterByCategory } from 'src/app/utils';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  smartphones: IProduct[] = [];
+  laptops: IProduct[] = [];
+  homeDecoration: IProduct[] = [];
+  skincare: IProduct[] = [];
+  fragrances: IProduct[] = [];
+  groceries: IProduct[] = [];
+
+  constructor(private productsService: GetProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data) => {
+      const dataList: IProduct[] = data.products;
+      this.smartphones = filterByCategory(dataList, 'smartphones');
+      this.laptops = filterByCategory(dataList, 'laptops');
+      this.homeDecoration = filterByCategory(dataList, 'home-decoration');
+      this.skincare = filterByCategory(dataList, 'skincare');
+      this.fragrances = filterByCategory(dataList, 'fragrances');
+      this.groceries = filterByCategory(dataList, 'groceries');
+    });
+  }
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { SwiperCategoryComponent } from 'src/app/components';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SwiperCategoryComponent],
 })
 export class HomeModule {}

01390011da7554a8a4fa0b43245c5f3a027192e5
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-01 19:57:36 -0500

    fix filter don't get the current product

--- src/app/modules/product-info/product-info.module.ts
+++ src/app/modules/product-info/product-info.module.ts
@@ -4,6 +4,9 @@ import { ProductInfoRoutingModule } from './product-routing.module';
 import { ProductInfoComponent } from './product-info.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { SwiperCategoryComponent } from 'src/app/components';
 
 @NgModule({
   declarations: [ProductInfoComponent],
@@ -12,6 +15,9 @@ import { MatIconModule } from '@angular/material/icon';
     ProductInfoRoutingModule,
     MatButtonModule,
     MatIconModule,
+    FormsModule,
+    MatDividerModule,
+    SwiperCategoryComponent,
   ],
 })
 export class ProductInfoModule {}
